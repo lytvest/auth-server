9ed03c1a88a1b188cb4827b296fbd33c414301a9
Дмитрий <bdm1999@yandex.ru>
2025-10-18 18:44:43 +0500

    fix bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.3.4"
+    id("org.springframework.boot") version "3.3.4" // или та, что указана
     id("io.spring.dependency-management") version "1.1.6"
 }
 
@@ -30,6 +30,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    // Явно добавим зависимость, на всякий случай
+    implementation("org.springframework.security:spring-security-oauth2-authorization-server")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.projectlombok:lombok")
